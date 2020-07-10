6c2c72e051c71c2c54169cadc15602307a1799f4
doanthuyennt <thuyentrandoan.ttd@gmail.com>
2020-07-10 04:34:48 +0000

    move configs

---
+++ configs/config_utils/conf.py
@@ -0,0 +1,28 @@
+import json
+from json_minify import json_minify
+class Conf:
+    def __init__(self, confPath):
+        """
+        load and store the configuration and update the 
+        object't dictinonary
+        """
+        conf = json.loads(json_minify(open(confPath).read()))
+        self.__dict__.update(conf)
+    def __getitem__(self,k):
+            # return the value associate with the supplied key
+            return self.__dict__.get(k, None)
+            
+test_config = Conf("/home/mmlab/git_workspace/yolov3_deepsort/metric_config/cosine_distance.config")
+test_config_2 = Conf("/home/mmlab/git_workspace/yolov3_deepsort/metric_config/euclidean_distance.config")
+test_config_3 = Conf("/home/mmlab/git_workspace/yolov3_deepsort/model_config/deep_sort.config")         
+if __name__ == '__main__':
+
+  print(test_config["max_cosine_distance"])
+  print(test_config_2["max_euclidean_distance"])
+  print(test_config_3["model_path"])
+  line_conf = {"x":100,"y":200}
+  
+  
+  with open('rules/detection_rule.json', 'w') as outfile:
+    json.dump(line_conf, outfile)
+  
